6c1c9782dd6103fd9f540d59e011ba249f761c58
Keith Thibodeaux <keith@railscoder.net>
2024-04-07 08:45:31 -0400

    use letter_opener_web

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,19 +1,25 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: proc { [404, {}, []] }
+
+  get 'health' => 'rails/health#show', as: :rails_health_check
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
   post 'sign_in', to: 'sessions#create'
   post 'sign_up', to: 'registrations#create'
+
   resources :sessions, only: %i[index show destroy]
   resource  :password, only: %i[edit update]
+
   namespace :identity do
     resource :email,              only: %i[edit update]
     resource :email_verification, only: %i[show create]
     resource :password_reset,     only: %i[create update]
   end
+
   namespace :authentications do
     resources :events, only: :index
   end
-  root to: proc { [404, {}, []] }
-
-  get 'health' => 'rails/health#show', as: :rails_health_check
 end
