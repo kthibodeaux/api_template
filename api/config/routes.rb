0959ae7782f62b1953179a89d53f533778f1ecdd
Keith Thibodeaux <keith@railscoder.net>
2024-04-07 06:55:04 -0400

    run auth generator
    bundle exec rails generate authentication --api --trackable --masqueradable

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  post "sign_in", to: "sessions#create"
+  post "sign_up", to: "registrations#create"
+  resources :sessions, only: [:index, :show, :destroy]
+  resource  :password, only: [:edit, :update]
+  namespace :identity do
+    resource :email,              only: [:edit, :update]
+    resource :email_verification, only: [:show, :create]
+    resource :password_reset,     only: [:new, :edit, :create, :update]
+  end
+  namespace :authentications do
+    resources :events, only: :index
+  end
   root to: proc { [404, {}, []] }
 
   get 'health' => 'rails/health#show', as: :rails_health_check
