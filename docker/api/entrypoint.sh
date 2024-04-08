e9e9f8361aa8067bf3f4dc5ce6c653c8f0c137bb
Keith Thibodeaux <keith@railscoder.net>
2024-04-08 15:44:59 -0400

    use rake task to determine if database exists

--- docker/api/entrypoint.sh
+++ docker/api/entrypoint.sh
@@ -7,23 +7,26 @@ bundle check || bundle install -j8
 bundle exec rails db:prepare
 
 if [ "$RESEED" = "true" ]; then
-  echo "Re-initializing development database"
-  bundle exec rails db:reset
-
   echo "Re-initializing test databases"
   bundle exec rails db:reset RAILS_ENV=test
-else
-  init_test_database=$(psql -lqt "$DATABASE_URL" | cut -d \| -f 1 | grep -w api_test)
-  init_development_database=$(psql -lqt "$DATABASE_URL" | cut -d \| -f 1 | grep -w api_development)
 
-  if test -z "$init_test_database"; then
+  echo "Re-initializing development database"
+  bundle exec rails db:reset
+else
+  bundle exec rails db:exists RAILS_ENV=test
+  init_test_database_status=$?
+
+  if [ $init_test_database_status -ne 0 ]; then
     echo "Initializing test databases"
     bundle exec rails db:setup RAILS_ENV=test
   else
     echo "Test databases already exist; skipping setup"
   fi
 
-  if test -z "$init_development_database"; then
+  bundle exec rails db:exists RAILS_ENV=development
+  init_development_database_status=$?
+
+  if [ $init_development_database_status -ne 0 ]; then
     echo "Initializing development database"
     bundle exec rails db:setup
   else
