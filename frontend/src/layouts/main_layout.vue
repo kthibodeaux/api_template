cb584ccf43e61b7fc03512344d126e94586d25a7
Keith Thibodeaux <keith@railscoder.net>
2024-04-09 06:28:35 -0400

    frontend: use snake case

---
+++ frontend/src/layouts/main_layout.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+</script>
+
+<template lang="pug">
+.container
+  nav.navbar(role="navigation" aria-label="main navigation")
+    .navbar-brand
+      a.navbar-item(href="/") API Template App
+      a.navbar-burger(role="button" data-target="navbar" @click="isOpen = !isOpen")
+        span(aria-hidden="true")
+        span(aria-hidden="true")
+        span(aria-hidden="true")
+        span(aria-hidden="true")
+    #navbar.navbar-menu(:class="{ 'is-active': isOpen }" :key="$route.name")
+      .navbar-start
+        RouterLink.navbar-item(:to="{ name: 'home' }") Home
+      .navbar-end
+
+.container
+  slot
+</template>
