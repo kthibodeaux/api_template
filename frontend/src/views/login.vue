0383036299b31e0d6a56f579834529598e164e06
Keith Thibodeaux <keith@railscoder.net>
2024-04-09 07:27:17 -0400

    frontend: login page template

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -1,8 +1,38 @@
 <script setup>
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+const rememberMe = ref(false)
+const error = ref(null)
+
+const login = () => {
+}
 </script>
 
 <template lang="pug">
 section.section
-  p hello login
-  RouterLink(:to="{ name: 'home' }") home
+  .columns.is-centered
+    .column.is-half
+      h1.title.has-text-centered Login
+      form(@submit.prevent="login")
+        .field.has-text-centered(v-if="error")
+          p.has-text-danger {{ error }}
+        .field
+          label.label Email Address
+          .control
+            input.input(type="text" v-model="email" placeholder="Enter your email address" required)
+        .field
+          label.label Password
+          .control
+            input.input(type="password" v-model="password" placeholder="Enter your password" required)
+        .field
+          .control
+            label.checkbox
+              input(type="checkbox" v-model="rememberMe")
+              |  Remember me
+        .field
+          .control
+            button.button.is-primary.is-fullwidth(type="submit")
+              | Login
 </template>
