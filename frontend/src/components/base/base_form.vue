c27359a26cf8bff54c1c385511c661ab0ef53217
Keith Thibodeaux <keith@railscoder.net>
2024-04-09 08:15:05 -0400

    frontend: extract base components

---
+++ frontend/src/components/base/base_form.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  error: { type: String, default: null },
+})
+
+const emit = defineEmits(['submit'])
+</script>
+
+<template lang="pug">
+form(@submit.prevent="emit('submit')")
+  .field.has-text-centered(v-if="error")
+    p.has-text-danger {{ error }}
+  slot
+</template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,4 +11,13 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+import BaseCheckbox from '@/components/base/base_checkbox.vue'
+app.component('BaseCheckbox', BaseCheckbox)
+import BaseForm from '@/components/base/base_form.vue'
+app.component('BaseForm', BaseForm)
+import BaseInput from '@/components/base/base_input.vue'
+app.component('BaseInput', BaseInput)
+import BaseSubmitButton from '@/components/base/base_submit_button.vue'
+app.component('BaseSubmitButton', BaseSubmitButton)
+
 app.mount('#app')
