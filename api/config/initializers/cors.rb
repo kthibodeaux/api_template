201ddf0de2d311ec74496bed63b82ee4bd334542
Keith Thibodeaux <keith@railscoder.net>
2024-04-11 05:53:10 -0400

    backend: only enable CORS if FRONTEND_URL is present

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,13 +7,15 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins ENV['FRONTEND_URL']
+if ENV['FRONTEND_URL']
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins ENV['FRONTEND_URL']
 
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head],
-             credentials: true
+      resource '*',
+               headers: :any,
+               methods: %i[get post put patch delete options head],
+               credentials: true
+    end
   end
 end
