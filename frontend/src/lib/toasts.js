633f9092dbdd3fdf9129c096a98863bc24714bc0
Keith Thibodeaux <keith@railscoder.net>
2024-04-12 06:12:12 -0400

    frontend: use notyf for toasts

--- frontend/src/lib/toasts.js
+++ frontend/src/lib/toasts.js
@@ -1,7 +1,42 @@
+import { Notyf } from 'notyf'
+import 'notyf/notyf.min.css'
+
+const notyf = new Notyf({
+  duration: 2000,
+  position: {
+    x: 'center',
+    y: 'top',
+  },
+  types: [
+    {
+      type: 'success',
+      dismissible: true,
+      duration: 2000,
+      icon: {
+        className: 'iconoir-check-circle-solid',
+        color: 'white',
+        tagName: 'i',
+      },
+      ripple: false,
+    },
+    {
+      type: 'error',
+      dismissible: true,
+      duration: 5000,
+      icon: {
+        className: 'iconoir-warning-triangle-solid',
+        color: 'white',
+        tagName: 'i',
+      },
+      ripple: false,
+    },
+  ],
+})
+
 export const successToast = function(message) {
-  alert(message)
+  notyf.success(message)
 }
 
 export const errorToast = function(message) {
-  alert(message)
+  notyf.error(message)
 }
