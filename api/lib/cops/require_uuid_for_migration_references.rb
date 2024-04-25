2724046d42b8a3a7d392e84e2c53a530c77af938
Keith Thibodeaux <keith@railscoder.net>
2024-04-25 07:58:57 -0400

    backend: add custom rubocop to warn when migration references doesnt use uuid

---
+++ api/lib/cops/require_uuid_for_migration_references.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class RequireUuidForMigrationReferences < RuboCop::Cop::Base
+  MESSAGE = 'Use `type: :uuid` for references in migrations.'
+
+  def on_send(node)
+    return unless migration?
+
+    receiver_node, method_name, *arg_nodes = *node
+    return unless method_name == :references
+
+    arg_nodes.each do |arg_node|
+      next unless arg_node.hash_type?
+
+      arg_node.pairs.each do |pair_node|
+        key, value = *pair_node
+
+        return if key && key.sym_type? && key.value == :type && value && value.sym_type? && value.value == :uuid
+      end
+    end
+
+    add_offense(node, message: MESSAGE)
+  end
+
+  private
+
+  def migration?
+    processed_source.buffer.name.include?('db/migrate/')
+  end
+end
