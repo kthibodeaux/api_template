79eefdcbc28f041b7c08553487d76a43f0bf2261
Keith Thibodeaux <keith@railscoder.net>
2024-05-11 11:15:16 -0400

    backend: combine after_update hooks for password

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -20,15 +20,12 @@ class User < ApplicationRecord
 
   normalizes :email, with: -> { _1.strip.downcase }
 
-  after_update if: :password_digest_previously_changed? do
-    sessions.where.not(id: Current.session).delete_all
-  end
-
   after_update if: :email_previously_changed? do
     events.create! action: 'email_changed'
   end
 
   after_update if: :password_digest_previously_changed? do
+    sessions.where.not(id: Current.session).delete_all
     events.create! action: 'password_changed'
   end
 
