7d2c962e0add63f937ff89b016c461c4409051c7
Keith Thibodeaux <keith@railscoder.net>
2024-05-11 11:13:56 -0400

    backend: log email changing actions correctly
    email changing will work by sending an email to the new address that
    contains an update link with a token. so by having the email address
    update it will already be verified and will not require additional
    verification

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -20,24 +20,16 @@ class User < ApplicationRecord
 
   normalizes :email, with: -> { _1.strip.downcase }
 
-  before_validation if: :email_changed?, on: :update do
-    self.verified = false
-  end
-
   after_update if: :password_digest_previously_changed? do
     sessions.where.not(id: Current.session).delete_all
   end
 
   after_update if: :email_previously_changed? do
-    events.create! action: 'email_verification_requested'
+    events.create! action: 'email_changed'
   end
 
   after_update if: :password_digest_previously_changed? do
     events.create! action: 'password_changed'
-  end
-
-  after_update if: %i[verified_previously_changed? verified?] do
-    events.create! action: 'email_verified'
   end
 
   def admin?
